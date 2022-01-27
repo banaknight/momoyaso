b8d620cc62e864425e42c374a476797f29f05d34
J Mochizuki <junya00mochizuki@gmail.com>
2022-01-27 15:47:55 +0900

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `momoyaso`,
+    description: `Junya Mochizukiの軌跡を記す。`,
+    author: `@Junya`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `momoyaso`,
+        short_name: `momoyaso`,
         start_url: `/`,
         background_color: `#663399`,
         // This will impact how browsers show your PWA/website
